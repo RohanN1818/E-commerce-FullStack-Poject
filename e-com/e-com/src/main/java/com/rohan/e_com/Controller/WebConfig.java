3f3a4a48ce7ea4e688ebba84ab855a2af4560d01
Rohan N <rohannrohann90@gmail.com>
2026-08-13 23:02:36 +0530

    add vercel domain to solve cors policy;

--- e-com/e-com/src/main/java/com/rohan/e_com/Controller/WebConfig.java
+++ e-com/e-com/src/main/java/com/rohan/e_com/Controller/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","http://localhost:5173", "https://e-commerce-full-stack-rohan.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
